394ef9f1f989467cfb78d6878349330702e96b6a
AGrachov <43968942+AGrachov@users.noreply.github.com>
2020-06-16 11:11:50 +0300

    Update index.php

--- Project/index.php
+++ Project/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Lab_1</title>
 </head>
 <body>
     <?php
@@ -19,87 +19,72 @@
         protected $surname;
         protected $role;
 
-        function __construct($arr) {
-            $this->login = $arr["login"];
-            $this->password = $arr["password"];
-            $this->name = $arr["name"];
-            $this->surname = $arr["surname"];
-            $this->role = $arr["role"];
+        function __construct($user) {
+            $this->login = $user["login"];
+            $this->password = $user["password"];
+            $this->role = $user["role"];
+            $this->name = $user["name"];
+            $this->surname = $user["surname"];
         }
+    };
+    class ClientClass extends Visiter{
         function getInfo(){
-            switch($this->role){
-                case"Manager":
-                     echo "Здравствуйте"." ".$this->role." ".$this->name." ".$this->surname.
-                      ". Вы можете на сайте изменять, удалять и создавать клиентов.";
-                     break;
-                case"Сlient":
-                     echo "Здравствуйте"." ".$this->role." ".$this->name." ".$this->surname.
-                        ". Вы можете на сайте просматривать информацию доступную пользователям.";
-                     break;
-                case"Admin":
-                    echo "Здравствуйте"." ".$this->role." ".$this->name." ".$this->surname.
-                        ". Вы можете на сайте делать всё.";
-                    break;
-            }
+            echo "Здравствуйте"." ".$this->role." ".$this->name." ".$this->surname.
+            ". Вы можете на сайте просматривать информацию доступную пользователям.";
         }
-        
-        };
-        class ManagerClass extends Visiter{
-            function isManager(){
-                return $this->role === 'manager';
-            }
-        };
-        class ClientClass extends Visiter{
-            function isClient(){
-                return $this->role === 'client';
-            }
-        };
-        class AdminClass extends Visiter{
-            function isAdmin(){
-                return $this->role === 'admin';
-            }
-        };
-         
-        $pass = $_POST["password"];
-        $log = $_POST["login"];
-
-    ?>
+    };
+    class ManagerClass extends Visiter{
+        function getInfo(){
+            echo "Здравствуйте"." ".$this->role." ".$this->name." ".$this->surname.
+            ". Вы можете на сайте изменять, удалять и создавать клиентов.";
+        }
+    };
+    class AdminClass extends Visiter{
+        function getInfo(){
+             echo "Здравствуйте"." ".$this->role." ".$this->name." ".$this->surname.
+            ". Вы можете на сайте делать всё.";
+        }
+    };
+?>
     <p>Введите логин и пароль:</p>
     <form method="post">
         <p>Логин: <input type="text" name="login" > </p>
         <p>Пароль: <input type="text" name="password" > </p>
         <input type="submit" value="Ввести">
     </form>
-    <div style="margin: 0 auto; border: 2px solid crimson; width: 300px; height: 200px">
-    <?php
-    if($pass || $log){
-    foreach($arr as $k =>$v){
-    if(($arr[$k]["login"] ==  $log) ||($arr[$k]["password"] == $pass) ){
-        $counter;
-        switch ($k) {
-            case "Manager":
-                $mane = new ManagerClass($v);
-                $mane->getInfo();
-                $counter++;
-                break;
-            case "Сlient":
-                $cli = new ClientClass($v);
-                $cli->getInfo();
-                $counter++;
-                break;
-            case "Admin":
-                $adm = new AdminClass($v);
-                $adm->getInfo();
-                $counter++;
-                break;
-        };
+    <div style=" border: 1px solid green; width: 300px; height: 200px">
+<?php
+    $password = $_POST["password"];
+    $login = $_POST["login"];
+
+    if($password || $login){
+        foreach($arr as $k =>$v){
+        if(($arr[$k]["login"] ==  $login) && ($arr[$k]["password"] == $password) ){
+            $counter;
+            $visiter;
+            switch ($k) {
+                case "Manager":
+                    $visiter = new ManagerClass($v);
+                    $counter++;
+                    break;
+                case "Сlient":
+                    $visiter = new ClientClass($v);
+                    $counter++;
+                    break;
+                case "Admin":
+                    $visiter = new AdminClass($v);
+                    $counter++;
+                    break;
+            };
         }; 
     };
-    if($counter == 0) {
-        echo "Вы неправильно ввели логин или пароль или вы не зарегестрированы";
-};
+        if($counter == 0) {
+            echo "Вы неправильно ввели логин или пароль или вы не зарегестрированы";
+        }else{
+            echo $visiter->getInfo();
+        };
     };
-        ?>
+?>
         </div>
 </body>
 </html>
